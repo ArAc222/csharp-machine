cd0ff9251b34ffa4f80592978b4bb8e83c4e1630
ArAc222 <105783863+ArAc222@users.noreply.github.com>
2025-11-26 12:13:37 +0100

    Create Basics.cs

---
+++ 01-Basics/Basics.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is an exercise");
+
+            Console.ReadLine();
+        }
+    }
+}
