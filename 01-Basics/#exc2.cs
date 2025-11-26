6977f02d768ed0e0727aabccd001d8ab1ad21edf
ArAc222 <105783863+ArAc222@users.noreply.github.com>
2025-11-26 12:22:29 +0100

    Create #exc2.cs

---
+++ 01-Basics/#exc2.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            double num2 = 55.55;
+            string sent = "This is strange";
+            bool isIt = false;
+            char let = 's';
+
+            Console.WriteLine($"{sent}, because my number {num1} is not equal to {num2} and meaning my {let}tatment is {isIt}");
+        }
+
+
+    }
+}
